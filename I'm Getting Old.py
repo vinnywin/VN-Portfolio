7eef60bb0497354db0fda39f18085be77b36bd98
Viet Nguyen <vinnywin@live.com>
2025-02-27 15:27:10 -0500

    Old man

---
+++ I'm Getting Old.py
@@ -0,0 +1,7 @@
+#This code shows me how old I am gonna be in 2050
+#20250227_NguyenViet_2-2
+myCurrentAge = 32
+currentYear = 2025
+myNewAge = myCurrentAge + (2050 - currentYear)
+print("My Current Age is " + str(myCurrentAge))
+print("I will be " + str(myNewAge) + " in 2050.")

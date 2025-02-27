04acb4aa2d0adfa8c14b2ac8910ae6ccba1a36ab
Viet Nguyen <vinnywin@live.com>
2025-02-27 15:53:51 -0500

    Furniture

---
+++ Furniture.py
@@ -0,0 +1,14 @@
+itemName = "TV Stand"
+retailPrice = 325.00
+wholesalePrice = 200.00
+profit = retailPrice - wholesalePrice
+salePrice = retailPrice - (retailPrice * .25)
+saleProfit = salePrice - wholesalePrice
+
+print("Item Name: " + itemName)
+print("Retail Price: $" + str(retailPrice))
+print("Wholesale Price: $" + str(wholesalePrice))
+print("Profit: $" + str(profit))
+print("Sale Price: $" + str(salePrice))
+print("Sale Profit: $" + str(saleProfit))
+print("20250227_NguyenViet_2-3")

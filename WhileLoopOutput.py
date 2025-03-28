03417e61b16dc30107eb67d9cd7a3951c7ddba32
Viet Nguyen <vinnywin@live.com>
2025-03-27 23:53:45 -0400

    doneski

---
+++ WhileLoopOutput.py
@@ -0,0 +1,12 @@
+# Initialize variables
+p = 2
+q = 4
+
+while p < q:
+    print("Audios")
+    r = 1
+    while r < q:
+        print("Audios")
+        r = r + 1
+    p = p + 1
+print("20250327_NguyenViet_5-1")

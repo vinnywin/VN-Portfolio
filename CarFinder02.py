0a6c9f48c7249285c5b8aa0a00b3ce993e11af40
Viet Nguyen <vinnywin@live.com>
2025-04-14 11:18:11 -0400

    CarFinder03

--- CarFinder02.py
+++ CarFinder02.py
@@ -1,7 +1,7 @@
-#List of authorized vehicles
-AllowedVehiclesList = [ 'Ford F-150', 'Chevrolet Silverado', 'Tesla CyberTruck', 'Toyota Tundra', 'Nissan Titan' ]
+# List of authorized vehicles
+AllowedVehiclesList = ['Ford F-150', 'Chevrolet Silverado', 'Tesla CyberTruck', 'Toyota Tundra', 'Nissan Titan']
 
-#Function to display the menu
+# Function to display the menu
 def display_menu():
     print("********************************")
     print("AutoCountry Vehicle Finder v0.2")
@@ -9,17 +9,26 @@ def display_menu():
     print("Please enter the number below from the following menu:")
     print("1. PRINT all Authorized Vehicles")
     print("2. SEARCH for a Vehicle")
-    print("3. Exit")
+    print("3. ADD a Vehicle to the Authorized List")
+    print("4. Exit")
     print("20250407_NguyenViet_Project0-2")
 
-#Function to search for a vehicle
+# Function to search for a vehicle
 def search_vehicle(vehicle_name):
     if vehicle_name in AllowedVehiclesList:
         print(f"{vehicle_name} is an authorized vehicle.")
     else:
         print(f"{vehicle_name} is not an authorized vehicle, if you received this in error please check the spelling and try")
 
-#Main program loop
+# Function to add a vehicle to the authorized list
+def add_vehicle(vehicle_name):
+    if vehicle_name in AllowedVehiclesList:
+        print(f"{vehicle_name} is already in the authorized list.")
+    else:
+        AllowedVehiclesList.append(vehicle_name)
+        print(f"{vehicle_name} has been added to the authorized list.")
+
+# Main program loop
 def main():
     while True:
         display_menu()
@@ -35,11 +44,15 @@ def main():
             search_vehicle(vehicle_name)
             print("\n")
         elif choice == "3":
+            vehicle_name = input("Please Enter the full Vehicle name you would like to add: ")
+            add_vehicle(vehicle_name)
+            print("\n")
+        elif choice == "4":
             print("Thank you for using the AutoCountry Vehicle Finder, good-bye!")
             break
         else:
-            print("Invalid input. Please enter 1, 2, or 3.")
+            print("Invalid input. Please enter 1, 2, 3, or 4.")
 
-#Run the program
+# Run the program
 main()
 print("20250407_NguyenViet_Project0-2")
